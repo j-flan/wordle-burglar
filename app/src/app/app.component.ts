961ce4dc162da3fadd04f01e6641d8cf197a8f90
j-flan <jhflanegan@gmail.com>
2022-04-07 11:04:18 -0400

    known positions, add back in removed bad letters

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -20,6 +20,30 @@ export class AppComponent {
   fourth: any = [];
   fifth: any = [];
 
+  firstKnown: string = '';
+  secondKnown:string = '';
+  thirdKnown: string = '';
+  fourthKnown:string = '';
+  fifthKnown: string = '';
+
+  knownLetters = [
+    this.firstKnown,
+    this.secondKnown,
+    this.thirdKnown,
+    this.fourthKnown,
+    this.fifthKnown
+  ]
+  unknownLetterPositions = [
+    this.first,
+    this.second,
+    this.third,
+    this.fourth,
+    this.fifth
+  ]
+
+  badLetterCache: any = []
+
+
   constructor(
     //public wordListService: WordListService
   ){
@@ -34,10 +58,20 @@ export class AppComponent {
     this.third = [];
     this.fourth = [];
     this.fifth = [];
+    this.firstKnown = '';
+    this.secondKnown = '';
+    this.thirdKnown = '';
+    this.fourthKnown = '';
+    this.fifthKnown = '';
     this.getTop(wordList);
   }
   filterBadLetters(){
-    this.filteredWords = _.filter(this.filteredWords, word=>{
+    if(this.badLetterCache.length && this.badLetterCache.length > this.badLetters.length){
+      const removedLetter: any = _.difference(this.badLetterCache.split(''), this.badLetters.split(''));
+      const addWords = _.filter(wordList, word=>_.includes(word, removedLetter))
+      this.filteredWords = _.concat(this.filteredWords, addWords);
+    }else{
+      this.filteredWords = _.filter(this.filteredWords, word=>{
         let letterNotIncluded: boolean = true
           _.forEach(this.badLetters, letter=>{
             if(_.includes(word, letter)){
@@ -46,9 +80,12 @@ export class AppComponent {
           })
         return letterNotIncluded
       });
+    }
+    this.badLetterCache = this.badLetters
     this.getTop(this.filteredWords);
   }
   filterPosition(list: any, index: number){
+    if(!list)return;
     this.filteredWords = _.filter(this.filteredWords, word=>{
         let letterNotIncluded: boolean = true
           _.forEach(list, letter=>{
@@ -60,8 +97,32 @@ export class AppComponent {
       });
     this.getTop(this.filteredWords);
   }
+  filterKnown(letter: any, index: number){
+    if(!letter){
+      this.handleRemovedIndex(index);
+      return;
+    }
+    this.filteredWords = _.filter(this.filteredWords, word=>word[index]==letter);
+    this.getTop(this.filteredWords);
+  }
+  reFilter(letter: any, index: number, list: any){
+    return _.filter(list, word=>word[index]==letter);
+  }
   getTop(allWords: any){
     const words = _.slice(allWords,0,this.LISTMAX)
     this.top.next([...words])
+  }
+  handleRemovedIndex(index: number){
+    this.filteredWords = wordList
+    _.forEach(this.knownLetters, letter=>{
+      if(letter)
+        this.filteredWords = this.reFilter(letter, index, this.filteredWords)
+    })
+    if(this.badLetters.length)
+      this.filterBadLetters();
+    _.forEach(this.unknownLetterPositions, (letters, index)=>{
+      if(letters.length) this.filterPosition(letters, index);
+    })
+    this.getTop(this.filteredWords);
   }
 }
