c99edf9e91624ccde43cc07a1f489659339d3fab
j-flan <jhflanegan@gmail.com>
2022-04-08 10:04:37 -0400

    tweaks, need to handle removing known position with other known positions again

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { BehaviorSubject, from, Subject, take } from 'rxjs';
-import {wordList} from './word-list';
-import * as _ from 'lodash'
+import { wordList } from './word-list';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  top: BehaviorSubject<any> = new BehaviorSubject([])
+  top: BehaviorSubject<any> = new BehaviorSubject([]);
   badLetters: any = [];
   filteredWords: any = [];
   LISTMAX = 50;
@@ -21,9 +21,9 @@ export class AppComponent {
   fifth: any = [];
 
   firstKnown: string = '';
-  secondKnown:string = '';
+  secondKnown: string = '';
   thirdKnown: string = '';
-  fourthKnown:string = '';
+  fourthKnown: string = '';
   fifthKnown: string = '';
 
   knownLetters = [
@@ -31,30 +31,28 @@ export class AppComponent {
     this.secondKnown,
     this.thirdKnown,
     this.fourthKnown,
-    this.fifthKnown
-  ]
+    this.fifthKnown,
+  ];
   unknownLetterPositions = [
     this.first,
     this.second,
     this.third,
     this.fourth,
-    this.fifth
-  ]
+    this.fifth,
+  ];
 
-  badLetterCache: any = []
+  badLetterCache: any = [];
 
-
-  constructor(
-    //public wordListService: WordListService
-  ){
-    this.filteredWords = wordList
-    this.getTop(wordList)
+  constructor() //public wordListService: WordListService
+  {
+    this.filteredWords = wordList;
+    this.getTop(wordList);
   }
-  reset(){
+  reset() {
     this.filteredWords = wordList;
     this.badLetters = [];
-    _.forEach(this.unknownLetterPositions, letters=>letters = []);
-    _.forEach(this.knownLetters, letter=>letter='');
+    _.forEach(this.unknownLetterPositions, (letters) => (letters = []));
+    _.forEach(this.knownLetters, (letter) => (letter = ''));
     this.getTop(wordList);
   }
 
@@ -63,83 +61,120 @@ export class AppComponent {
   //not included
   //unknownposition
 
-  filterKnown(letter: any, index: number){
-    if(!letter){
-      this.knownLetters[index] = ''
+  filterKnown(letter: any, index: number) {
+    if (!letter) {
+      this.knownLetters[index] = '';
       this.handleRemovedKnownIndex(index);
       return;
     }
-    this.filteredWords = _.filter(this.filteredWords, word=>word[index]==letter);
+    this.filteredWords = _.filter(
+      this.filteredWords,
+      (word) => word[index] == letter
+    );
     this.knownLetters[index] = letter;
     this.getTop(this.filteredWords);
   }
-  handleRemovedKnownIndex(index: number){
-    this.filteredWords = wordList
-    _.forEach(this.knownLetters, letter=>{
-      if(letter)
-        this.filteredWords = this.reFilterIndex(letter, index, this.filteredWords)
-    })
-    if(this.badLetters.length)
-      this.filterBadLetters();
+  handleRemovedKnownIndex(index: number) {
+    this.filteredWords = wordList;
+    _.forEach(this.knownLetters, (letter) => {
+      if (letter)
+        this.filteredWords = _.filter(this.filteredWords, (word) => word[index] == letter);
+    });
+    if (this.badLetters.length) this.filterBadLetters();
     this.scanUnkown();
     this.getTop(this.filteredWords);
   }
 
-  filterBadLetters(){
-    if(this.badLetterCache.length && this.badLetterCache.length > this.badLetters.length){
-      const removedLetter: any = _.difference(this.badLetterCache.split(''), this.badLetters.split(''));
-      const addWords = _.filter(wordList, word=>_.includes(word, removedLetter))
-      this.filteredWords = _.concat(this.filteredWords, addWords);
-      this.scanUnkown();
-    }else{
-      const conflictLetter = _.filter(this.knownLetters, letter=>_.find(this.badLetters.split(''), badLetter=>badLetter==letter));
-      if(conflictLetter.length){
+  filterBadLetters() {
+    if (
+      this.badLetterCache.length &&
+      this.badLetterCache.length > this.badLetters.length
+    ) {
+      this.handleRemovedBadLetter();
+    } else {
+      const conflictLetter = _.filter(this.knownLetters, (letter) =>
+        _.find(this.badLetters.split(''), (badLetter) => badLetter == letter)
+      );
+      if (conflictLetter.length) {
         console.log('conflict: ', conflictLetter);
-        this.badLetters = _.filter(this.badLetters.split(''), letter=>letter!=conflictLetter);
+        this.badLetters = _.filter(
+          this.badLetters.split(''),
+          (letter) => letter != conflictLetter
+        );
       }
-      if(this.badLetters)
-      console.log('filter bad letters: ', this.badLetters.split(''))
+      if (this.badLetters.length){
+        console.log('filter bad letters: ', this.badLetters.split(''));
         this.filteredWords = _.filter(this.filteredWords, word=>{
-            return _.includes(word, this.badLetters[this.badLetters.length-1]);
+          let letterNotIncluded: boolean = true
+            _.forEach(this.badLetters, letter=>{
+              if(_.includes(word, letter) && !_.find(this.knownLetters, knownLetter=>knownLetter == letter)){
+                letterNotIncluded = false;
+              }
+            })
+          return letterNotIncluded
         });
+      }
     }
-    this.badLetterCache = this.badLetters
+    this.badLetterCache = this.badLetters;
     this.getTop(this.filteredWords);
   }
 
-  filterPosition(list: any, index: number){
-    if(!list)return;
-    this.filteredWords = _.filter(this.filteredWords, word=>{
-      return _.find(list, letter=>(word[index] != letter && _.includes(word, letter)));
+  handleRemovedBadLetter(){
+    const removedLetter: any = _.difference(
+      this.badLetterCache.split(''),
+      this.badLetters.split('')
+    );
+    const addWords = _.filter(wordList, (word) =>
+      _.includes(word, removedLetter)
+    );
+    this.filteredWords = _.concat(this.filteredWords, addWords);
+    this.scanUnkown();
+  }
+
+  filterPosition(list: any, index: number) {
+    if (list.length < this.unknownLetterPositions[index]?.length){
+      this.handleRemovedPosition(list, index)
+    }else{
+      console.log('have list: ', list.split(''), index);
+      this.filteredWords = _.filter(this.filteredWords, word=>{
+        let letterNotIncluded: boolean = true
+          _.forEach(list, letter=>{
+            if(word[index] == letter){
+              letterNotIncluded = false;
+            }
+          })
+        return letterNotIncluded
+      });
+    }
+    this.unknownLetterPositions[index] = list ? list : [];
+    console.log('have unknown: ', this.unknownLetterPositions);
+    this.getTop(this.filteredWords);
+  }
+
+  handleRemovedPosition(list: any, index: number) {
+    const removedLetter: any = _.difference(
+      this.unknownLetterPositions[index].split(''),
+      list
+    );
+    console.log('have removed letter position, ', removedLetter, index, list);
+    if(removedLetter && !_.find(this.badLetters, letter=> letter == removedLetter)){
+      const addWords = _.filter(wordList, (word) =>{
+        return _.includes(word[index], removedLetter)
+      });
+      this.filteredWords = _.concat(this.filteredWords, addWords);
+    }
+  }
+
+  scanUnkown() {
+    _.forEach(this.unknownLetterPositions, (letters, index) => {
+      if (letters.length) this.filterPosition(letters, index);
     });
-    this.getTop(this.filteredWords);
   }
 
-  reFilterIndex(letter: any, index: number, list: any){
-    return _.filter(list, word=>word[index]==letter);
+  getTop(allWords: any) {
+    const words = _.slice(allWords, 0, this.LISTMAX);
+    this.top.next([...words]);
   }
 
-  getTop(allWords: any){
-    const words = _.slice(allWords,0,this.LISTMAX)
-    this.top.next([...words])
-  }
-
-  handleRemovedIndex(index: number){
-    this.filteredWords = wordList
-    _.forEach(this.knownLetters, letter=>{
-      if(letter)
-        this.filteredWords = this.reFilterIndex(letter, index, this.filteredWords)
-    })
-    if(this.badLetters.length)
-      this.filterBadLetters();
-    this.scanUnkown()
-    this.getTop(this.filteredWords);
-  }
-
-  scanUnkown(){
-    _.forEach(this.unknownLetterPositions, (letters, index)=>{
-      if(letters.length) this.filterPosition(letters, index);
-    })
-  }
 
 }
